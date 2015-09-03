2a156c499c7e6d433384adcfc37e13fe97886bd2
Nicolas Barbé <nicolas.barbe@magnolia-cms.com>
2015-09-03 15:45:12 +0200

    Add FindAll and FindById

--- mongo.go
+++ mongo.go
@@ -24,6 +24,21 @@ func NewMongo(cs string, db string) *Mongo {
   }
 }
 
+func (this *Mongo) FindAll(collectionName string, result interface{})  {
+  err := this.database.C(collectionName).Find(nil).All(result)
+
+  if err != nil {
+    log.Print(err)
+  }
+}
+
+func (this *Mongo) FindById(collectionName string, id interface{}, result interface{})  {
+  err := this.database.C(collectionName).FindId(id).One(result)
+  if err != nil {
+    log.Print(err)
+  }
+}
+
 func (this *Mongo) Exists(collectionName string, id interface{}) bool {
   collection := this.database.C(collectionName)
   count, err := collection.FindId(id).Limit(1).Count()
